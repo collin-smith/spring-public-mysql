eaa23071d6f350e5e0b5a873ac5941913c47b13b
Collin Smith <collin.smith@gmail.com>
2024-03-06 15:17:55 -0700

    Initial checkin for a Spring Boot application interacting with a Public RDS MySQL instance with Secrets Manager in a 3 AZ VPC

---
+++ src/main/java/com/example/springpublicmysql/SpringPublicMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpublicmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPublicMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPublicMysqlApplication.class, args);
+	}
+
+}
